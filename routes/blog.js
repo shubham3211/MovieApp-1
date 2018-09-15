5c5047efabff301d8f7c3be87a28c34a94b4bfbc
shubham <kumarsinghshubham32111@gmail.com>
2018-09-16 05:04:06 +0530

    changes made

--- routes/blog.js
+++ routes/blog.js
@@ -16,11 +16,11 @@ route.post('/form',(req,res,next)=>{
 });
 
 route.use((req,res)=>{
-  let blog = handlebars.compile(fs.readFileSync('/home/shubham/MovieApp/views/BlogMovie.hbs').toString('utf-8'));
-  let blogCss = fs.readFileSync('/home/shubham/MovieApp/views/css/main.css');
+  let blog = handlebars.compile(fs.readFileSync('../views/BlogMovie.hbs').toString('utf-8'));
+  let blogCss = fs.readFileSync('../views/css/main.css');
   blog = blog(body);
-  fs.writeFileSync('/home/shubham/MovieApp/reviewSite/index.html',blog);
-  fs.writeFileSync('/home/shubham/MovieApp/reviewSite/blog.css',blogCss);
+  fs.writeFileSync('../reviewSite/index.html',blog);
+  fs.writeFileSync('../reviewSite/blog.css',blogCss);
   res.redirect('/github-oauth');
 });
 
